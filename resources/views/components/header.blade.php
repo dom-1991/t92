55dacf7339fd61324b787b6f8ccdab68f505e23f
Dom <dom-1991@gmail.com>
2022-12-28 23:21:15 +0700

    update

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="box-search border border-secondary border-1 position-relative pe-5 ps-3">
                     <form action="{{ route('homepage') }}">
                         <input type="text" class="border-0 w-100" name="search" placeholder="Tìm kiếm..." value="{{ request()->search }}">
-                        <button class="icon position-absolute btn-secondary text-white">
+                        <button class="icon position-absolute btn btn-secondary text-white">
                             <i class="fas fa-search"></i>
                         </button>
                     </form>
